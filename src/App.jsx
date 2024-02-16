60e89fe3208f49f635a9709dfad244e7d2cd842f
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-02-16 23:46:21 +0100

    route name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import EmailTemplate from './pages/email';
-import Acceptance from './pages/acceptance';
+import Acceptances from './pages/acceptance';
 import Rejections from './pages/rejection';
 import Schedule from './pages/schedule';
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<EmailTemplate />} />
-      <Route path='/acceptance' element={<Acceptance />} />
+      <Route path='/acceptance' element={<Acceptances />} />
       <Route path='/rejection' element={<Rejections />} />
       <Route path='/schedule' element={<Schedule />} />
     </Routes>
