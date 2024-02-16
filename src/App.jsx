57caa2accc005b0f67cfdb7f1a5f1ea924279301
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-02-16 23:45:39 +0100

    App file changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import EmailTemplate from './pages/email';
 import Acceptance from './pages/acceptance';
-import Rejection from './pages/rejection';
+import Rejections from './pages/rejection';
 import Schedule from './pages/schedule';
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<EmailTemplate />} />
       <Route path='/acceptance' element={<Acceptance />} />
-      <Route path='/rejection' element={<Rejection />} />
+      <Route path='/rejection' element={<Rejections />} />
       <Route path='/schedule' element={<Schedule />} />
     </Routes>
   );
